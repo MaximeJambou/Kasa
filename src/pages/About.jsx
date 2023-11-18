a050c5011d8cbf14c98bad4d2bde74978c15f8d9
Maxime Jambou~ <maxime.jambou@gmail.com>
2023-11-18 16:01:39 +0100

    Collapse corrected

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -10,16 +10,23 @@ const About = () => {
 
             <div className="about__container">
                 <Collapse title="Fiabilité" >
-                    Lorem ipsum dolor sit amet. 
+                    Les annonces postées sur Kasa garantissent une fiabilité totale.
+                    Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées par nos équipes.
                 </Collapse>
                 <Collapse title="Respect" >
-                    Lorem ipsum dolor sit amet.
+                    La bienveillance fait partie des fondatrices de Kasa.
+                    Tout comportement discriminatoire du voisinage entraînera uen exclusion de notre plateforme.
                 </Collapse>
                 <Collapse title="Service" >
-                    Lorem ipsum dolor sit amet.
+                    La bienveillance fait partie des fondatrices de Kasa.
+                    Tout comportement discriminatoire du voisinage entraînera uen exclusion de notre plateforme.
                 </Collapse>
                 <Collapse title="Sécurité" >
-                Lorem ipsum dolor sit amet..
+                    La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement
+                    correspond aux critères de sécurité établis par nos services.
+                    En laissant une note aussi bien à l'hôte qu'au
+                    locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. 
+                    Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes.
                 </Collapse>
             </div>
         </>
